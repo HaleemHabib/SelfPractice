fb8268b8d0d347c467bcf2b20e08464eb6f71dd7
Haleem Habib <haleem.habib07@gmail.com>
2022-05-09 21:29:14 -0400

    changes after break

--- src/SelfGitHubClass.java
+++ src/SelfGitHubClass.java
@@ -4,6 +4,11 @@ public class SelfGitHubClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a = 10;
+		int b = 20;
+		 int c = a+b;
+		 
+		 System.out.println(c);
 	}
 
 }
