203f4984bf4a304db298384785a91bc045d8ae47
Haleem Habib <haleem.habib07@gmail.com>
2022-06-21 13:33:36 -0400

    new changes added

--- src/SelfGitHubClass.java
+++ src/SelfGitHubClass.java
@@ -4,9 +4,9 @@ public class SelfGitHubClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int a = 10;
-		int b = 20;
-		 int c = a+b;
+		int a = 505050;
+		int b = 100000;
+		 int c = a*b;
 		 
 		 System.out.println(c);
 	}
